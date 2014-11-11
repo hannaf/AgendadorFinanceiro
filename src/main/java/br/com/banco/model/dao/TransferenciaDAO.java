93815bc93393aa66ddb538d1ed481d0dc2b3dba1
hannaf <hanna_f@ymail.com>
2014-11-11 21:44:27 -0200

    Ajuste classe TransferenciaDAO

--- src/main/java/br/com/banco/model/dao/TransferenciaDAO.java
+++ src/main/java/br/com/banco/model/dao/TransferenciaDAO.java
@@ -3,9 +3,6 @@ package br.com.banco.model.dao;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.bean.RequestScoped;
-import javax.websocket.Session;
-
 import br.com.banco.model.Transferencia;
 
 /**
@@ -13,31 +10,20 @@ import br.com.banco.model.Transferencia;
  * @author Hanna
  *
  */
-@RequestScoped
 public class TransferenciaDAO {
 	
 	/**
-	 * Session
+	 * Lista transferência
 	 */
-	private Session session;
+	private List<Transferencia> transferencias;
 	
-
 	/**
 	 * Construtor
 	 */
 	public TransferenciaDAO() {
-		this(null);
+		transferencias = new ArrayList<Transferencia>();
 	}
 	
-	/**
-	 * Construtor
-	 */	
-	public TransferenciaDAO(Session session) {
-		this.session = session;
-		List<Transferencia> transferencias = new ArrayList<Transferencia>();
-		session.
-	}
-
 	/**
 	 * Cadastra transferências
 	 * @param transferencia
