37eb5a916c9fd6fd9a19dee62d14f9392b511e50
hannaf <hanna_f@ymail.com>
2014-11-12 23:39:54 -0200

    Inicio configuraçao JPA + Hibernate, anotação das entidades

--- src/main/java/br/com/banco/model/TipoTransferencia.java
+++ src/main/java/br/com/banco/model/TipoTransferencia.java
@@ -2,12 +2,20 @@ package br.com.banco.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * 
  * @author Hanna
  *
  *
  */
+@Entity
+@Table(name="tipo_transferencia")
 public class TipoTransferencia implements Serializable {
 	
 	private static final long serialVersionUID = -6698923607071728883L;
@@ -15,17 +23,22 @@ public class TipoTransferencia implements Serializable {
 	/**
 	 * Atributo identificador
 	 */
+	@Id
+	@GeneratedValue
+	@Column(name="id_tipo_transferencia")
 	private Integer id;
 	
 	/**
 	 * Nome do tipo de transferencia
 	 */
+	@Column(name="nm_tipo_transferencia")
 	private String nome;
 	
 	/**
 	 * Descrição do tipo transferencia
 	 * Esse campo deve armazenar as regras de cobrança do tipo de transferencia 
 	 */
+	@Column(name="ds_tipo_transferencia")
 	private String descricao;
 
 	public Integer getId() {
--- src/main/java/br/com/banco/model/Transferencia.java
+++ src/main/java/br/com/banco/model/Transferencia.java
@@ -4,9 +4,15 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 /**
@@ -14,6 +20,8 @@ import javax.validation.constraints.Size;
  * @author Hanna
  *
  */
+@Entity
+@Table(name="transferencia")
 public class Transferencia implements Serializable {
 
 	private static final long serialVersionUID = 1053016088998833390L;
@@ -21,6 +29,9 @@ public class Transferencia implements Serializable {
 	/**
 	 * Atributo identificador
 	 */
+	@Id
+	@GeneratedValue
+	@Column(name="id_transferencia")
 	private Integer id;
 	
 	/**
@@ -28,6 +39,7 @@ public class Transferencia implements Serializable {
 	 */
 	@NotNull(message = "{campo.obrigatorio}")
 	@Size(min=5 , max=6, message="{conta.invalida}")
+	@Column(name="conta_origem")
 	private String contaOrigem;
 	
 	/**
@@ -35,6 +47,7 @@ public class Transferencia implements Serializable {
 	 */
 	@NotNull(message = "{campo.obrigatorio}")
 	@Size(min=5 , max=6, message="{conta.invalida}")
+	@Column(name="conta_destino")
 	private String contaDestino;
 	
 	/**
@@ -42,18 +55,27 @@ public class Transferencia implements Serializable {
 	 */
 	@NotNull(message = "{campo.obrigatorio}")
 	@Digits(integer=12, fraction=2, message="{valor.invalido}")
+	@Column(name="vl_transferencia")
 	private BigDecimal valor;
 	
 	/**
 	 * Taxa sobre a transferência
 	 */
+	@Column(name="taxa_transferencia")
 	private BigDecimal taxa;
+	
+	/**
+	 * Dada agendada para a transferência
+	 */
+	@NotNull(message = "{campo.obrigatorio}")
+	@Column(name="dt_transferencia")
+	private Date dataAgendamento;
 	
 	/**
 	 * Tipo de transferência
 	 */
-	@NotNull(message = "{campo.obrigatorio}")
-	private Date dataAgendamento;
+	@ManyToOne
+	@JoinColumn(name="id_tipo_transferencia")
 	private TipoTransferencia tipo;
 
 	public Integer getId() {
